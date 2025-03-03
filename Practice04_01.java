f578e736aa50a1fe46b663009f049df2cc160af5
haruka0429 <pudding.p3208@gmail.com>
2025-03-03 22:44:09 +0900

    Add files via upload

---
+++ Practice04_01.java
@@ -0,0 +1,23 @@
+public class Practice04_01 {
+    public static void main(String[] args) {
+        byte var_01 = 2;
+        long var_02 = 9876543210L;
+        float var_03 = 3.8F;
+        double var_04 = 1.9960717;
+
+        System.out.println(var_01);
+        System.out.println(var_02);
+        System.out.println(var_03);
+        System.out.println(var_04);
+
+        int var_05 = 5;
+        int var_06 = 7;
+        boolean var_bool;
+
+        var_bool = (var_05 < var_06);
+        
+        System.out.println(var_bool);
+
+    }
+    
+}
