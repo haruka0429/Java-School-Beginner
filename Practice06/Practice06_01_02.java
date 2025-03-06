bd824a791979cf5b3a4e53acb97b5fcf858be27a
haruka0429 <pudding.p3208@gmail.com>
2025-03-06 22:33:20 +0900

    Add files via upload

---
+++ Practice06/Practice06_01_02.java
@@ -0,0 +1,10 @@
+package Practice06;
+public class Practice06_01_02 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int c = 7;
+
+        System.out.println(b * c - a * c);
+    } 
+}
---
+++ Practice06/Practice06_02_02.java
@@ -0,0 +1,14 @@
+package Practice06;
+public class Practice06_02_02 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a + 5 + 8 * 3);
+        //　問１
+        a++;
+        System.out.println(a);
+        //　問２
+        a--;
+        System.out.println(a);
+    }
+    
+}
